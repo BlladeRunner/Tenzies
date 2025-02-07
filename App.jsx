8f0bb4bcc1c04cb1ef987fb69e9871048dbb62f6
Blladerunner <rufatahmedik@gmail.com>
2025-02-07 20:58:00 +0100

    roll dice button adding

--- App.jsx
+++ App.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import Die from "./Die";
 
 export default function App() {
-  const [dice, setDie] = React.useState(generateAllNewDice());
+  const [dice, setDice] = React.useState(generateAllNewDice());
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+  }
+
+  function rollDice() {
+    setDice(generateAllNewDice());
   }
 
   const diceElement = dice.map((num) => <Die value={num} />);
@@ -12,6 +16,9 @@ export default function App() {
   return (
     <main>
       <div className="dice-container">{diceElement}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
