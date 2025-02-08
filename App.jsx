d990f276c93e1d1f69e824e25060a01811fb3fa7
Blladerunner <rufatahmedik@gmail.com>
2025-02-08 21:53:19 +0100

    implenemting logs to own unique ID property

--- App.jsx
+++ App.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import { useState } from "react";
 import Die from "./Die";
+import { nanoid } from "nanoid";
 
 export default function App() {
-  const [dice, setDice] = React.useState(generateAllNewDice());
+  function hold(id) {
+    dieObj.id;
+  }
+
+  console.log(hold);
+
+  const [dice, setDice] = useState(generateAllNewDice());
+
   function generateAllNewDice() {
-    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+    return new Array(10).fill(0).map(() => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    }));
   }
 
   function rollDice() {
     setDice(generateAllNewDice());
   }
 
-  const diceElement = dice.map((num) => <Die value={num} />);
+  const diceElements = dice.map((dieObj) => (
+    <Die key={dieObj.id} value={dieObj.value} isHeld={dieObj.isHeld} />
+  ));
 
   return (
     <main>
-      <div className="dice-container">{diceElement}</div>
+      <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={rollDice}>
         Roll
       </button>
